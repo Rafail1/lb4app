2e2cbdb3b14e5af3bf805b9f4d29d2990faac675
Rafail1 <ya.rafon-92@ya.ru>
2019-05-27 10:04:30 +0300

    first

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -9,9 +9,38 @@ export class UserController {
       @repository(UserRepository) private userRepository: UserRepository,
       @repository(UserRoleRepository) private userRoleRepository: UserRoleRepository,
   ) {}
-
-  @post('/users/login')
-  async login(@requestBody() credentials: Credentials) {
+  @post('/users/authenticate')
+  async authenticate(@requestBody() user: User) {
+    if(!this.userRepository.checkUser(user)) {
+      throw new HttpErrors.BadRequest('Bad User');
+    }
+    const existUser = await this.userRepository.findOne({where:{id: user.id}});
+    if (existUser) {
+      await this.userRepository.updateById(existUser.id, user);
+      const roles = await this.userRoleRepository.find({where: {userId: user.id}});
+      const {first_name, photo_url, last_name, username} = user;
+      return {
+        token:user.hash,
+        first_name, last_name,photo_url, username,
+        roles: roles.map(r => r.roleId)
+      };
+    } else {
+      this.userRepository.create(user).then((createdUser:User) => {
+        const {first_name, photo_url, last_name, username} = createdUser;
+        return {
+          token:createdUser.hash,
+          first_name, last_name,photo_url, username,
+          roles: []
+        };
+      }, error => {
+        console.error(error);
+        throw new HttpErrors.BadRequest('Bad User');
+      })
+    }
+    return;
+  }
+  @post('/users/authenticateByHash')
+  async loginByHash(@requestBody() credentials: Credentials) {
     console.log(credentials.hash);
     const user = await this.userRepository.findOne({where: {hash: credentials.hash}});
     if (!user) throw new HttpErrors.Unauthorized('Invalid credentials');
@@ -24,14 +53,4 @@ export class UserController {
       roles: roles.map(r => r.roleId)
     };
   }
-  @post('/users/register')
-  async register(@requestBody() user: User) {
-    if(!this.userRepository.checkUser(user)) {
-      throw new HttpErrors.BadRequest('Bad User');
-    }
-    const userExist = await this.userRepository.count({id: user.id});
-    if (userExist) throw new HttpErrors.Unauthorized('User Exist');
-    return;
-  }
-
 }

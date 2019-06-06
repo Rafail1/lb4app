f8dd55c03144d8d9cef10b814584ee4d6a6f6f12
Rafail1 <ya.rafon-92@ya.ru>
2019-06-06 11:29:24 +0300

    just

--- src/telegram-bot/repositories/bot.repository.ts
+++ src/telegram-bot/repositories/bot.repository.ts
@@ -15,7 +15,9 @@ export class BotRepository extends DefaultCrudRepository<
   async create(entity: Bot, options?: Options): Promise<Bot> {
     const bot = new TelegramBot(entity.apiKey, { polling: false });
     const me = await bot.getMe();
-
+    if (entity.active) {
+      //todo запуск бота на удаленном сервере
+    }
     entity.id = me.id;
     entity.first_name = me.first_name;
     entity.username = me.username;

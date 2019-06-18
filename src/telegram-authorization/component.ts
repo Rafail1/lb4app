94671b6f14617c93be312dd559a58d3d8c1e89b2
Rafail1 <ya.rafon-92@ya.ru>
2019-06-18 10:38:19 +0300

    auth_refactor

--- src/telegram-authorization/component.ts
+++ src/telegram-authorization/component.ts
@@ -1,8 +1,8 @@
-import {Component, ProviderMap} from '@loopback/core';
-import {MyAuthActionProvider, MyAuthBindings} from "./providers/auth-action.provider";
-import {MyAuthMetadataProvider} from "./providers/auth-metadata.provider";
-import {MyAuthStrategyProvider} from "./providers/auth-strategy.provider";
-import {AuthenticationBindings} from '@loopback/authentication';
+import { Component, ProviderMap } from '@loopback/core';
+import { MyAuthActionProvider, MyAuthBindings } from "./providers/auth-action.provider";
+import { MyAuthMetadataProvider } from "./providers/auth-metadata.provider";
+import { MyAuthStrategyProvider } from "./providers/auth-strategy.provider";
+import { AuthenticationBindings } from '@loopback/authentication';
 
 export class TelegramAuthorizationComponent implements Component {
     providers?: ProviderMap;
--- src/telegram-authorization/decorators/auth.decorator.ts
+++ src/telegram-authorization/decorators/auth.decorator.ts
@@ -2,20 +2,14 @@ import { SecuredType } from "../permission-key";
 import { MyAuthenticationMetadata } from "../types";
 import { MethodDecoratorFactory } from '@loopback/core';
 import { AUTHENTICATION_METADATA_KEY } from "@loopback/authentication";
-import { Class, Repository, Model } from '@loopback/repository';
 
 export function secured(
     type: SecuredType = SecuredType.IS_AUTHENTICATED,
     roles: string[] = [],
-    model?: Repository<Model> | undefined,
-    id?: string | undefined
 ) {
-    // we will use a custom interface. more on this below
     return MethodDecoratorFactory.createDecorator<MyAuthenticationMetadata>(AUTHENTICATION_METADATA_KEY, {
         type,
         roles: roles || [],
-        model: model || undefined,
-        id: id || '',
         options: undefined,
         strategy: "",
     });
